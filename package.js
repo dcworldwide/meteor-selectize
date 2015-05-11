438eff94d53734629faeae32c3ba1bb277946e20
Danijel <dan.baternik@gmail.com>
2015-05-11 10:39:46 +1000

    Removed css

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: "jeremy:selectize",
+  name: "dcworldwide:selectize-unstyled",
   summary: "Selectize is a hybrid of a textbox and <select> element used for tagging, contact lists, and more.",
   version: '0.12.1_3',
-  git: 'https://github.com/jshimko/meteor-selectize.git'
+  git: 'https://github.com/dcworldwide/meteor-selectize.git'
 });
 
 Package.onUse(function(api) {
